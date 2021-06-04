cce087bbbbdbbf99949ca568b2b8cc5b1af85b81
Michael Clautice <michaelclauticeusa@gmail.com>
2021-06-04 16:51:00 -0400

    Mike modified 2 files & created 1 new file Fri.6.04.21.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # NetNinja_NodeJS_Lesson_14
-Michael Clautice, Richmond, VA<br>
-The Net Ninja - NodeJS Tutorial for Beginners<br>
+Michael Clautice, Richmond, VA
+The Net Ninja - NodeJS Tutorial for Beginners
 Lesson 14 - Readable Streams
 
 https://www.youtube.com/watch?v=E3tTzx0Qoj0&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp&index=14
 
 • How to create a readable stream & use it to read data / files. 
 
-• W/ a readable stream you don't have to wait for the full file to be stored in memory before we start to manipulate the data.
+• W/ a readable stream you don't have to wait for the full file to be stored in memory before manipulating the data.
--- app.js
+++ app.js
@@ -2,3 +2,21 @@
 // The Net Ninja - NodeJS Tutorial for Beginners 
 // Lesson 14 - Readable Streams
 // -------
+// req the fs mod
+const fs = require('fs');
+
+// declare a variable that is assigned the value of...
+// the encoded text content of readMe.txt
+const myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+
+// fs.createReadStream() inherits from the events.EventEmitter()...
+// the ability to listen for an event
+// myReadStream.on() event listener listens for a data event...
+// ea data event fires a cb function
+// that takes a chunk of data from my myReadStream
+// & con.logs a mssg, the actual data chunk & a line break
+myReadStream.on('data', function(chunk){
+    console.log('\nnew chunk received:\n');
+    console.log(chunk);
+    console.log('\n')
+});
